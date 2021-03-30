6f35b73be5c0a42c7eba6af95119a7f24f256062
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-03-30 23:48:25 +0900

    [#3] 프로젝트 생성

---
+++ src/test/java/com/board/board/BoardApplicationTests.java
@@ -0,0 +1,15 @@
+package com.board.board;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
