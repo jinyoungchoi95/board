a818f6caa2226bb2aa0a16f7dc9b8a2ebf248a52
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-05-11 16:05:39 +0900

    [#10] test: 카테고리별 게시글 목록 조회 테스트코드 추가

--- src/integration_test/java/com/board/board/post/service/PostServiceTest.java
+++ src/integration_test/java/com/board/board/post/service/PostServiceTest.java
@@ -1,23 +1,17 @@
 package com.board.board.post.service;
 
+import com.board.board.category.domain.Category;
+import com.board.board.category.domain.CategoryRepository;
 import com.board.board.post.domain.Post;
 import com.board.board.post.domain.PostRepository;
 import com.board.board.post.dto.PostTitleResponseDto;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,14 +24,26 @@ class PostServiceTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @AfterEach
+    void cleanup(){
+        postRepository.deleteAll();
+        categoryRepository.deleteAll();
+    }
+
     @Test
     @DisplayName("페이지 번호가 1이 들어왔을 때 첫번째 페이지를 반환하여야 한다.")
     void testFindPostTitleByPageNumIs1() {
 
         //given
         Integer pageNum = 1;
+        categoryRepository.save(Category.builder().name("category").build());
+        Category category = categoryRepository.findAll().get(0);
+
         for(int i=1; i<=20; i++){
-            postRepository.save(Post.builder().title("title" + i).content("content" + i).build());
+            postRepository.save(Post.builder().title("title" + i).content("content" + i).category(category).build());
         }
 
         //when
@@ -48,6 +54,44 @@ class PostServiceTest {
         assertThat(dtos.getSize()).isEqualTo(15);
         assertThat(dtos.getNumber()).isEqualTo(0);
         assertThat(dtos.getContent().get(1).getTitle()).isEqualTo("title2");
+        assertThat(dtos.getContent().get(1).getCategoryName()).isEqualTo("category");
+        assertThat(dtos.nextPageable().getPageNumber()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("특정 카테고리의 게시글 Pagenation만 반환되어야 한다.")
+    void testFindPostTitleByPageNumAndCategory() {
+
+        //given
+        Integer pageNum = 1;
+        categoryRepository.save(Category.builder().name("category1").build());
+        categoryRepository.save(Category.builder().name("category2").build());
+
+        Category category1 = categoryRepository.findAll().get(0);
+        for(int i=1; i<=20; i++){
+            postRepository.save(Post.builder()
+                    .title("title" + i)
+                    .content("content" + i)
+                    .category(category1)
+                    .build());
+        }
+        Category category2 = categoryRepository.findAll().get(1);
+        for(int i=21; i<=40; i++){
+            postRepository.save(Post.builder()
+                    .title("title" + i)
+                    .content("content" + i)
+                    .category(category2)
+                    .build());
+        }
+
+        //when
+        Page<PostTitleResponseDto> dtos = postService.findPostTitleByPageNumAndCategory(pageNum, "category2");
+
+        //then
+        assertThat(dtos.getSize()).isEqualTo(15);
+        assertThat(dtos.getNumber()).isEqualTo(0);
+        assertThat(dtos.getContent().get(1).getTitle()).isEqualTo("title22");
+        assertThat(dtos.getContent().get(1).getCategoryName()).isEqualTo("category2");
         assertThat(dtos.nextPageable().getPageNumber()).isEqualTo(1);
     }
 }
