46a57a4b250da13e9937d466be1c68b0e1c19771
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-05-11 16:04:57 +0900

    [#10] feat: 카테고리별 게시글 목록 조회 service 추가
    - findPostTitleByPageNumAndCategory : 페이지 번호와 카테고리 이름을 받았을 때 해당되는 페이지 리스트를 반환
    
    - PostRepository
    	- findByCategory() : 카테고리와 pageable에 따라 Page 리스트 select
    - CategoryRepository
    	- findByName() : 카테고리명으로 카테고리 select

--- src/main/java/com/board/board/post/domain/PostRepository.java
+++ src/main/java/com/board/board/post/domain/PostRepository.java
@@ -1,8 +1,10 @@
 package com.board.board.post.domain;
 
+import com.board.board.category.domain.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Page<Post> findByCategory(Category category, Pageable pageable);
 }
