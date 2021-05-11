0f3caee9abe9943419393ac9bb5a471cd5adaf27
jinyoungchoi <jinyoungchoi95@gmail.com>
2021-05-11 16:02:06 +0900

    [#10] feat: Post, Category 양방향 매핑 추가
    - PostMapper : dto가 Category의 name을 받아야하므로 타겟을 재설정

--- src/main/java/com/board/board/post/mapper/PostMapper.java
+++ src/main/java/com/board/board/post/mapper/PostMapper.java
@@ -3,15 +3,14 @@ package com.board.board.post.mapper;
 import com.board.board.post.domain.Post;
 import com.board.board.post.dto.PostTitleResponseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper
 public interface PostMapper {
 
     PostMapper postMapper = Mappers.getMapper(PostMapper.class);
 
+    @Mapping(target = "categoryName", source = "category.name")
     PostTitleResponseDto entityToTitleDto(Post post);
-    List<PostTitleResponseDto> entityToTitleDto(List<Post> post);
 }
